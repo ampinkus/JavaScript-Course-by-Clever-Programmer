3ed1b8cb42ce7f3b7b9006d94d530aa5e277014b
ampinkus <ampinkus@gmail.com>
2022-11-17 17:14:59 -0300

    objects video completed

--- JavaScript-Course-by-Clever-Programmer--main/functions/sum/exercise/script.js
+++ JavaScript-Course-by-Clever-Programmer--main/functions/sum/exercise/script.js
@@ -8,23 +8,34 @@
   ES6 Syntax (Arrow function): const add = () => {}
 */
 
-function add(){
+
   //Add function here
-}
+  let sumArrow = (a, b) => a + b;
 
-function sub(){
+
+
   //Subtract function here
-}
+  let substractArrow = (a, b) => a - b;
 
-function div(){
+
+
   //Divide function here
-}
+  let divideArrow = (a, b) => {
+    if (b == 0) {
+      return "divide by zero";
+    } else return a / b;
+  };
 
-function mul(){
   //Multiply function here
-}
+  let multiplyArrow = (a, b) => a * b;
 
-console.log('hello from the SUM exercise')
+
+console.log("hello from the SUM exercise");
 /* 
   TODO: create a function that console logs the result of any of the above operations.
 */
+
+console.log(sumArrow(10, 5));
+console.log(substractArrow(10, 5));
+console.log(divideArrow(10, 0));
+console.log(multiplyArrow(10, 5));
